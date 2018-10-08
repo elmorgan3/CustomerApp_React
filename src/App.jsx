179c1b47c0ce8a38f36262e5ca93b0365b1b7013
Morgan Quintana <morgan_becoulet@gemvision.com>
2018-10-08 08:40:05 +0200

    Added Switch metod of react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomeContainer from './containers/HomeContainer';
 import CustomersContainer from './containers/CustomersContainer';
 import CustomerContainer from './containers/CustomerContainer';
